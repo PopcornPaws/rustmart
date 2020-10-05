ccac2e241d2a93836dc636b8ddea269cb6d24f8f
Mark Melczer <melczer7@gmail.com>
2020-10-05 14:15:26 +0200

    Implementing product detail page.

--- src/pages/product_detail.rs
+++ src/pages/product_detail.rs
@@ -5,6 +5,47 @@ use yew::format::Json;
 use yew::prelude::*;
 use yew::services::fetch::FetchTask;
 
-// TODO
-struct State {}
+pub struct State {
+    product: Option<Product>,
+    get_products_error: Option<Error>,
+    get_products_loaded: bool,
+}
+
+pub struct ProductDetail {
+    props: Props,
+    state: State,
+    link: ComponentLink<Self>,
+    task: Option<FetchTask>,
+}
+
+#[derive(Properties, Clone)]
+pub struct Props {
+    pub id: i32,
+}
+
+pub enum Msg {
+    GetProduct,
+    GetProductSuccess(Product),
+    GetProductError(Error),
+}
+
+impl Component for ProductDetail {
+    type Message = Msg;
+    type Properties = Props;
+
+    fn create(props: Self::Properties, link: ComponentLink<Self>) -> Self {
+        link.send_message(Msg::GetProduct);
+
+        Self {
+            props,
+            state: State {
+                product: None,
+                get_product_error: None,
+                get_products_loaded: false,
+            },
+            link,
+            task: None,
+        }
+    }
+}
 
